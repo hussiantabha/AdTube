c8858d22f939448ec28559467d77c78a68f7fc54
hussiantabha <tabhahusain@gmail.com>
2022-05-02 16:40:41 +0530

    Added: All the feature of playlist management
    Now user can make a playlist and add video to it. User can also delete playlist.

--- src/pages/DisplayVideo.js
+++ src/pages/DisplayVideo.js
@@ -1,103 +1,224 @@
-import React, { useEffect, useState, useContext } from "react";
-import Navbar from "../components/Navbar";
-import { Link, useParams } from "react-router-dom";
-import { v4 as uuid } from "uuid";
-import { BiLike } from "react-icons/bi";
+import React, { useState, useEffect, useContext } from "react";
 import { VideoContext } from "../context/Data";
+import NormalNavbar from "../components/NormalNavbar";
+import { BiLike } from "react-icons/bi";
+import { useLocation, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const DisplayVideo = () => {
+  const { videoState, dispatch } = useContext(VideoContext);
+  const [playlist, setPlaylist] = useState("");
+  const [allPlaylist, setAllPlayist] = useState([]);
   const [video, setVideo] = useState({});
-  const { videoState } = useContext(VideoContext);
-  const { videoId } = useParams();
-  const url = `/api/video/${videoId}`;
+  const [playlistModal, setPlaylistModal] = useState(false);
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIyMzAyOTRlOS05NzFiLTQxYTQtYmY2Ny04MDU3YjJhYmIwNTUiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.cz0FYZb3tlsVRwm38QKhsGIs9Ovn5UTk3_fttTrU9G8";
-  useEffect(async () => {
-    const response = await fetch(url);
-    const convertedJSON = await response.json();
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxMjVkM2MzNy01MjcwLTQ5NjgtODQ0MC1iZTM3ZDFhNTE5OGUiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.DPS9hLIaykSx9V9SwXsOhWgWQ7nk8MtTyumcWlbYamM";
+  const param = useParams();
+  const getVideo = async () => {
+    const getData = await fetch(`/api/video/${param.videoId}`);
+    const convertedJSON = await getData.json();
     setVideo(convertedJSON.video);
-  });
-  const likeVideo = async (video) => {
-    const postData = await fetch("/api/user/likes", {
+  };
+
+  const findPlaylist = async () => {
+    const getData = await fetch("/api/user/playlists", {
+      method: "GET",
+      headers: {
+        authorization: token,
+      },
+    });
+    const convertedJSON = await getData.json();
+    dispatch({
+      type: "playlistData",
+      payload: { value: convertedJSON.playlists },
+    });
+  };
+  const postPlaylist = async () => {
+    const postData = await fetch("/api/user/playlists", {
       method: "POST",
       headers: {
+        "Content-type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({
+        playlist: {
+          title: playlist,
+        },
+      }),
+    });
+    const getData = await fetch("/api/user/playlists", {
+      method: "GET",
+      headers: {
+        authorization: token,
+      },
+    });
+    const convertedJSON = await getData.json();
+    const filtered = await convertedJSON.playlists.filter(
+      (item) => item.title === playlist
+    );
+    const postData2 = await fetch(`/api/user/playlists/${filtered[0]._id}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
         authorization: token,
       },
       body: JSON.stringify({
         video,
       }),
     });
-    if (postData.status === 201) {
-      const convertedJSON = await postData.json();
-      console.log(convertedJSON);
+    if (postData2.status === 201) {
+      toast.success("Added Successfully", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
     }
-    if (postData.status === 409) {
-      console.log("Already in Liked Videos");
-    }
+    setPlaylist("");
+    setPlaylistModal(false);
   };
+  const addVideoToPlaylist = async () => {
+    allPlaylist.map((item) => {
+      const playlistVideoState = videoState.playlist.filter(
+        (element) => element.title === item
+      );
+      const innerFunc = async () => {
+        const postData = await fetch(
+          `/api/user/playlists/${playlistVideoState[0]._id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+              authorization: token,
+            },
+            body: JSON.stringify({
+              video,
+            }),
+          }
+        );
+        if (postData.status === 201) {
+          toast.success("Added Successfully", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      };
+      innerFunc();
+      return;
+    });
+    setPlaylistModal(false);
+  };
+  useEffect(() => {
+    findPlaylist();
+  });
+  useEffect(() => {
+    getVideo();
+  }, []);
+  useEffect(() => {
+    getVideo();
+  }, [param.videoId]);
+  //console.log(videoState);
   return (
     <>
-      <Navbar />
-      <section className="video-section-2">
-        <section>
-          <main>
-            <div className="video-container">
-              <iframe
-                width="100%"
-                height="100%"
-                src={video.video_url}
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen="1"
-                className="video-iframe"
-              ></iframe>
-              <div className="video-container-content">
-                <h2>{video.title}</h2>
-                <div className="video-container-icons">
-                  <div>
-                    <h5>{`${video.views} . ${video.uploadedDate}`}</h5>
-                  </div>
-                  <div>
-                    <div className="video-icon">
-                      <button onClick={() => likeVideo(video)}>
-                        <BiLike /> Like
-                      </button>
-                    </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+      />
+      <NormalNavbar />
+      <div className={playlistModal ? "playlist-modal" : "playlist-modal-hide"}>
+        <div className="playlist-modal-top-container">
+          <input
+            type="text"
+            onChange={(e) => setPlaylist(e.target.value)}
+            value={playlist}
+          />
+          <button className="btn btn-primary-outline" onClick={postPlaylist}>
+            Add
+          </button>
+        </div>
+        {videoState.playlist.map((element) => {
+          return (
+            <div className="individual-playlist">
+              <input
+                type="checkbox"
+                value={element.title}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setAllPlayist((prev) => [...prev, e.target.value]);
+                  } else {
+                    setAllPlayist(
+                      allPlaylist.filter((item) => item !== e.target.value)
+                    );
+                  }
+                }}
+                name={element.title}
+              />
+              <p>{element.title}</p>
+            </div>
+          );
+        })}
+        <button onClick={addVideoToPlaylist}>Add To Playlist</button>
+      </div>
+      <section>
+        <main>
+          <div className="video-container">
+            <iframe
+              width="100%"
+              height="100%"
+              src={video.video_url}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen="1"
+              className="video-iframe"
+            ></iframe>
+
+            <div className="video-container-content">
+              <h2>{video.title}</h2>
+              <div className="video-container-icons">
+                <div>
+                  <h5>{`${video.views} . ${video.uploadedDate}`}</h5>
+                </div>
+                <div>
+                  <div className="video-icon">
+                    <button
+                      onClick={() => likeVideo(video)}
+                      className="btn btn-primary-outline btn-like"
+                    >
+                      <BiLike /> Like
+                    </button>
+                    <button
+                      className="btn btn-primary-outline"
+                      onClick={() => {
+                        setPlaylistModal((prev) => !prev);
+                      }}
+                    >
+                      Add to Playlist
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          </main>
-        </section>
-        <main>
-          <section className="sidebar-video-card-container">
-            {videoState.data.map(
-              ({ title, thumbnail, creator, views, uploaded, _id }) => {
-                return (
-                  <>
-                    <div className="sidebar-video-card">
-                      <Link to={`/video/${_id}`}>
-                        <div className="sidebar-video-img-container">
-                          <img src={thumbnail} className="sidebar-video-img" />
-                        </div>
-                        <div className="sidebar-video-content-container">
-                          <h4>{title}</h4>
-                          <span>{creator}</span>
-                          <div>
-                            <span>{`${views} views . ${uploaded} `}</span>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </>
-                );
-              }
-            )}
-          </section>
+          </div>
         </main>
       </section>
     </>
   );
 };
-// `/api/video/${videoId}`;
+
 export default DisplayVideo;
--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import NormalNavbar from "../components/NormalNavbar";
+import { VideoContext } from "../context/Data";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Playlist = () => {
-  return <div>Playlist</div>;
+  const { videoState, dispatch } = useContext(VideoContext);
+  console.log(videoState);
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxMjVkM2MzNy01MjcwLTQ5NjgtODQ0MC1iZTM3ZDFhNTE5OGUiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.DPS9hLIaykSx9V9SwXsOhWgWQ7nk8MtTyumcWlbYamM";
+  const deletePlaylist = async (id) => {
+    const postData = await fetch(`/api/user/playlists/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        authorization: token,
+      },
+    });
+    if (postData.status === 200) {
+      const convertedJSON = await postData.json();
+      dispatch({
+        type: "playlistData",
+        payload: { value: convertedJSON.playlists },
+      });
+      toast.success("Deleted Successfully", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+      />
+      <NormalNavbar />
+      {videoState.playlist.length > 0 ? (
+        videoState.playlist.map((item) => {
+          console.log(item.videos);
+          return (
+            <>
+              <main className="playlist-container">
+                <div className="playlist-top-container">
+                  <h2>{item.title}</h2>
+                  <button
+                    className="btn btn-primary-outline"
+                    onClick={() => deletePlaylist(item._id)}
+                  >
+                    Delete Playlist
+                  </button>
+                </div>
+                <main className="playlist-video-card-container">
+                  {item.videos.map(
+                    ({
+                      _id,
+                      title,
+                      thumbnail,
+                      avatar_url,
+                      creator,
+                      views,
+                      uploaded,
+                      duration,
+                    }) => {
+                      return (
+                        <div className="video-card-container">
+                          <Link to={`/video/${_id}`}>
+                            <div className="video-img-container">
+                              <img src={thumbnail} className="video-img" />
+                              <span className="video-duration-badge">
+                                {duration}
+                              </span>
+                            </div>
+                            <div className="video-content-container">
+                              <img
+                                src={avatar_url}
+                                className="creator-avatar"
+                              />
+                              <div className="video-text-container">
+                                <h3>{title}</h3>
+                                <p>{creator}</p>
+                                <p>{`${views} views . ${uploaded}`}</p>
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
+                      );
+                    }
+                  )}
+                </main>
+              </main>
+            </>
+          );
+        })
+      ) : (
+        <h1 className="center-align">No playlist found</h1>
+      )}
+    </>
+  );
 };
 
 export default Playlist;
